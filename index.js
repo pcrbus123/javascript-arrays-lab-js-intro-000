927b4965de9c6c8d9942466b52538b6e7a530f25
Pnina Breslav <pcrbus123@gmail.com>
2018-10-28 12:40:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]//define your array here
 console.log(kittens)
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+kittens.push ("Ray");
+return kittens;
+};
